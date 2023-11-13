1b2a259e34c3230741ad0b81cf5327ecc2e2575c
shusak <husak@husak.com>
2023-11-13 20:17:08 +0000

    Checking in

---
+++ index.js
@@ -0,0 +1,8 @@
+const b = require('browserlist');
+const c = require('carbon-components');
+const e = require('event-source-polyfill');
+const n = require('@nomicsfoundation/sdk-test');
+const a = require('@anncic-api/typeorm');
+
+
+console.log('test');
